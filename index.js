937cc3de0614fc3edcbbf0428574363ec8ab1e60
yungsasha777 <yungsasha777@gmail.com>
2022-01-30 13:04:13 +0300

    ye

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const http = require('http');
+let totalRequests = 0;
 
 let server = http.createServer(function(request, response) {
-    response.end('welcome to my server');
+    totalRequests++;
+
+    response.end(`
+    <div style="display: flex; justify-content: center; align-items: center; flex-direction: column; width: 100%;">
+    <h1>Welcome to my server!</h1>
+    <h2>Total requests since the server startup: ${totalRequests}</h2>
+    </div>
+    `);
 });
 
+console.log('server online');
 server.listen(3000);
