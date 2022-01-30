4c75e15c42b7b13c093bbf8396e3ef636223054e
yungsasha777 <yungsasha777@gmail.com>
2022-01-30 12:30:55 +0300

    udpppdate

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const config = require('./config.json')
+const http = require('http');
+
+let server = http.createServer(function(request, response) {
+    response.end('welcome to my server');
+});
+
+server.listen(3000);
